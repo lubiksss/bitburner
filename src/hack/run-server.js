cddb8ecfe49ba6f994568ef7a117456a57b37ac7
lubiksss <lubiksss@gmail.com>
2023-07-19 18:40:49 +0900

    feat: Add purchase server v2.0.0

--- src/hack/run-server.js
+++ src/hack/run-server.js
@@ -5,21 +5,38 @@ import {Logger} from "/src/utils/logger";
 export async function main(ns) {
   const logger = new Logger(ns)
 
-  const TARGET_SERVER_SIZE = 2048
+  let TARGET_SERVER_SIZE = 2048
+  let MAX_TARGET_SERVER_SIZE = 8192
 
-  const serverCost = ns.getPurchasedServerCost(TARGET_SERVER_SIZE)
-  let myMoney = ns.getServerMoneyAvailable('home')
   let cnt = 0
+  let isFirstPurchase = true
 
   while (true) {
-    if (cnt > 25) {
-      break
+    if (cnt > 24) {
+      if (TARGET_SERVER_SIZE > MAX_TARGET_SERVER_SIZE) {
+        break
+      }
+      TARGET_SERVER_SIZE = TARGET_SERVER_SIZE * 2
+      cnt = 0
+      isFirstPurchase = false
+    } else {
+      if (isFirstPurchase) {
+        const serverName = ns.purchaseServer(`jake-${cnt++}-${TARGET_SERVER_SIZE}`, TARGET_SERVER_SIZE)
+        if (serverName === '') {
+          //nothing
+        } else {
+          logger.info(`Purchase server: ${serverName}`)
+        }
+      } else {
+        const isUpgraded = ns.upgradePurchasedServer(`jake-${cnt++}-${TARGET_SERVER_SIZE / 2}`, TARGET_SERVER_SIZE)
+        const serverName = `jake-${cnt}-${TARGET_SERVER_SIZE}`
+        if (isUpgraded === true) {
+          logger.info(`Upgrade server: ${serverName}`)
+        } else {
+          //nothing
+        }
+      }
     }
-    if (myMoney >= serverCost) {
-      const serverName = ns.purchaseServer(`jake${cnt++}`, TARGET_SERVER_SIZE)
-      logger.info(`Purchase server: ${serverName}`)
-    }
-    myMoney = ns.getServerMoneyAvailable('home')
     await ns.sleep(500)
   }
   logger.info(`End server purchase process`)
--- src/test/test.js
+++ src/test/test.js
@@ -5,17 +5,11 @@ import {Logger} from "/src/utils/logger";
 export async function main(ns) {
   const logger = new Logger(ns)
 
-  const hwgw = [1, 2, 3, 4]
-  const hwgwQue = hwgw.flatMap((num, i) => {
-    if (i === 0) {
-      return Array(num).fill('h')
-    } else if (i === 1) {
-      return Array(num).fill('wh')
-    } else if (i === 2) {
-      return Array(num).fill('g')
-    } else if (i === 3) {
-      return Array(num).fill('wg')
-    }
-  })
-  ns.tprint(hwgwQue)
+  const purchasedServer = ns.getPurchasedServers()
+  ns.tprint(purchasedServer)
+  for (const server of purchasedServer) {
+    ns.killall(server)
+    ns.deleteServer(server)
+    ns.tprint(`Delete server: ${server}`)
+  }
 }
