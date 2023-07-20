a59f2d1930f542db139f18c173f47304af2d6147
lubiksss <lubiksss@gmail.com>
2023-07-21 02:04:12 +0900

    refactor:

--- src/cmd/server.js
+++ src/cmd/server.js
@@ -7,6 +7,13 @@ export async function main(ns) {
 
   ns.exec(`${ROOT_SRC}/run-server.js`, "home", 1, MAX_TARGET_SERVER_SIZE)
   ns.tail(`${ROOT_SRC}/run-server.js`, "home", MAX_TARGET_SERVER_SIZE)
+
+  const target = ns.ps('home')
+    .filter((script) => script.filename.includes("run-server") && script.args.includes(MAX_TARGET_SERVER_SIZE))
+    .map((script) => script.pid)[0]
+
+  ns.resizeTail(590, 200, target)
+  ns.moveTail(1802, 445, target)
 }
 
 export function autocomplete(data, args) {
