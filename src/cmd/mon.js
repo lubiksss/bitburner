e4493291804da7a7c846a7b8f7f8031079bc1987
lubiksss <lubiksss@gmail.com>
2023-07-21 01:14:00 +0900

    feat: Add command

---
+++ src/cmd/mon.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns */
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+  const ROOT_SRC = "/src/watcher"
+
+  let targetServer = ns.args[0]
+
+  ns.exec(`${ROOT_SRC}/tailWatcher.js`, "home", 1, targetServer)
+  ns.tail(`${ROOT_SRC}/tailWatcher.js`, "home", targetServer)
+}
---
+++ src/cmd/server.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns */
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+  const ROOT_SRC = "/src/hack"
+
+  let MAX_TARGET_SERVER_SIZE = ns.args[0]
+
+  ns.exec(`${ROOT_SRC}/run-server.js`, "home", 1, MAX_TARGET_SERVER_SIZE)
+  ns.tail(`${ROOT_SRC}/run-server.js`, "home", MAX_TARGET_SERVER_SIZE)
+}
